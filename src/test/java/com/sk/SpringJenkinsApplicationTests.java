45cb60c7dceb72ebe5bca0e7ae33da410359eb13
2020.ysandeep@gmail.com <you@example.com>
2022-09-13 22:44:33 -0700

    add my application

---
+++ src/test/java/com/sk/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.sk;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger log = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		log.info("Test Case Executed.. ");
+		assertEquals(true, true);
+	}
+
+}
